1e026edf2d6a27e55063529ff90d9ae1872a9328
Cirill Florenz  Dalangin <florenzdalangin@gmail.com>
2018-08-04 21:24:35 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -21,11 +21,15 @@ function pressIt() {
 function submitIt() {
   $('form').on('submit', 
     function() {
-      if ($("input.first").val() === "correct") {
-        alert('your form is going to be submitted now');
-        return;
-      }
-    })
+$("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+});
+})
 }
 $(document).ready(function(){
 
